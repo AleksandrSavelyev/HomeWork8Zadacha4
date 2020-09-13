d8ff3558c21f994db1ea2f15fa3541c247327a58
AleksandrSavelyev <punk558721973@gmail.com>
2020-09-14 00:28:16 +0300

    soxraneniye

---
+++ HomeWork8Zadacha4/Money.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork8Zadacha4
+{
+    class Money
+    {
+        private int first;
+        private int second;
+        private int price;
+        public Money() { }
+        public Money(int first, int second) { }
+        public int First
+        {
+            get { return first; }
+            set
+            {
+                if (value > 0)
+                    first = value;
+                else
+                    first = 0;
+            }
+        }
+        public int Second
+        {
+            get { return second; }
+            set
+            {
+                if (value > 0)
+                    second = value;
+                else
+                    second = 0;
+            }
+        }
+        public int IsMoney
+        {
+            get
+            {
+                int totalMoney = first * second;
+                return totalMoney;
+            }
+        }
+        public int Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if (value > 0)
+                    price = value;
+                else
+                    price = 1;
+            }
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"num {first}\t {second}");
+        }
+        public int GetProd()
+        {
+            int prod = IsMoney / price;
+            return prod;
+        }
+    }
+}
---
+++ HomeWork8Zadacha4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HomeWork8Zadacha4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money user = new Money();
+            Console.Write("количество купюр:\t");
+            user.First = CheVal();
+            Console.Write("наминал купюр:\t");
+            user.Second = CheVal();
+            Console.Write("стоимость товара:\t");
+            user.Price = CheVal();
+            user.GetInfo();
+            Console.WriteLine($"{user.First} купюр наминалом {user.Second}Ррублей составляет {user.IsMoney}Рублей");
+            Console.WriteLine($"на {user.Price}рублей можно купить {user.GetProd()}");
+        }
+        static int CheVal()
+        {
+            for (; ; )
+            {
+                if (int.TryParse(Console.ReadLine(), out int a))
+                    return a;
+                else
+                    Console.Write("неверные данные повторите ввод");
+            }
+        }
+
+    }
+}
